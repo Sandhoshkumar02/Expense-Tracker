e0703511221bea84f1f60f75af97fd6b65dae87e
Sandhoshkumar02 <sandhoshkumar604@gmail.com>
2026-04-03 22:31:39 +0530

    CrossOrigin is changed successfully

--- backend/src/main/java/com/sandy/expensetracker/expensetracker/controller/ExpenseController.java
+++ backend/src/main/java/com/sandy/expensetracker/expensetracker/controller/ExpenseController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ExpenseController {
 
     private final ExpenseRepository expenseRepository;
